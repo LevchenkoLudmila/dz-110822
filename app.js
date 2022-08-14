007088808a7086b0b3459da549b1de0ce3103941
”LevchenkoLudmila” <milayahuliganka737@gmail.com>
2022-08-15 01:45:19 +0300

    di

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
 const express = require('express');
 const server = express();
 
+const fs = require('fs');
+const mainRouter = require('./routes/main.js');
+
 server.set('view engine', 'ejs');
 server.set('views','./views');
+
 server.use(express.static('./public'));
-
-server.get('/',(req,res) =>{
-   const arrey = [
-      {id:1,
-      prodact: 'book',
-      img: '/1.jpg'
-   },
-   {id:2,
-      prodact: 'book',
-      img: '/1.jpg'
-   },
-   {id:3,
-      prodact: 'book',
-      img: '/1.jpg'
-   }
-]
-   res.render('home',{arrey: arrey});
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
+server.use('/', mainRouter);
 
 server.listen (3000);
---
+++ routes/main.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+server.get('/',(req,res) =>{
+   const clous = [
+      {id:1, prodact: 'mens boots', color: 'waite', img: './img/1.jpg'},
+      {id:2, prodact: 'wimen boots', color: 'red', img: './img/1.jpg'},
+      {id:3, prodact: 'chaild boots', color: 'green', img: './img/1.jpg'}
+   ]
+   res.render('main',{clous: clous});
+});
