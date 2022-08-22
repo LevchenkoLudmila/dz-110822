1d64e0ee4c4f7a1b3869130e86585d3673402fba
”LevchenkoLudmila” <milayahuliganka737@gmail.com>
2022-08-22 16:58:55 +0300

    work all

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 
 const mainRouter = require('./routes/main.js');
 const mainListJsonRouter = require('./routes/list.js');
-const routerMan = require('./routes/mens.js');
-const routerWimen = require('./routes/wimen.js');
 
 server.use(express.static('./public'));
 
@@ -15,6 +13,5 @@ server.set('views','./views');
 
 server.use('/', mainRouter);
 server.use('/list', mainListJsonRouter);
-server.use('/wimen', routerWimen);
 
 server.listen (3000);
